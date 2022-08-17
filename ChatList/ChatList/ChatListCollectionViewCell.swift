576ddb641968d337c775ea9156621b258dc444bc
KayAhn0126 <dkseotn123@gmail.com>
2022-08-17 17:53:46 +0900

    refactor: 프로젝트 완성

---
+++ ChatList/ChatList/ChatListCollectionViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  ChatListCollectionViewCell.swift
+//  ChatList
+//
+//  Created by Kay on 2022/08/16.
+//
+
+import UIKit
+
+class ChatListCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var conversationLabel: UILabel!
+    @IBOutlet weak var dataLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        thumbnailImageView.layer.cornerRadius = 10
+    }
+    
+    func configure(_ chatModel: ChatModel) {
+        thumbnailImageView.image = UIImage(named: chatModel.name)
+        nameLabel.text = chatModel.name
+        conversationLabel.text = chatModel.chat
+        dataLabel.text = formattedDateString(dateString: chatModel.date)
+    }
+    
+    func formattedDateString(dateString: String) -> String {
+        // String -> Date -> String
+        // 2022-04-01 > 4/1
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        
+        // 문자열 -> date
+        if let date = formatter.date(from: dateString) {
+            formatter.dateFormat = "M/d"
+            return formatter.string(from: date)
+        } else {
+            return ""
+        }
+    }
+    
+}
--- ChatList/ChatList/ViewController.swift
+++ ChatList/ChatList/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var chatList: [ChatModel] = ChatModel.list
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        chatList = chatList.sorted(by: { chat1, chat2 in
+            return chat1.date > chat2.date
+        })
     }
-
-
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        chatList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ChatListCollectionViewCell", for: indexPath) as? ChatListCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        let chat = chatList[indexPath.item]
+        cell.configure(chat)
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 100)
+    }
+}
